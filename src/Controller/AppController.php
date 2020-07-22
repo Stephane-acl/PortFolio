a485f62eae37c884a89c166b28aaa4e45a42a78c
Stephane-acl <stephaneacl@gmail.com>
2020-07-22 22:08:26 +0200

    header on the homepage done

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,9 +16,13 @@ class AppController extends AbstractController
 {
     /**
      * @Route("/", name="homeIndex")
+     * @param UserRepository $userRepository
+     * @return Response
      */
-    public function homeIndex(): Response
+    public function homeIndex(UserRepository $userRepository): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('home.html.twig', [
+            'users'=>$userRepository->findAll()
+        ]);
     }
 }
