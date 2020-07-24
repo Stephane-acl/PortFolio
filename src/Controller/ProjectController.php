6d171b7ffb693ea1e374fc667ed51f2e5cd8b986
Stephane-acl <stephaneacl@gmail.com>
2020-07-24 11:01:02 +0200

    footer is dynamic

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,12 +16,16 @@ class ProjectController extends AbstractController
     /**
      * @Route("/{slug}", name="project_show", methods={"GET", "POST"})
      * @param Project $project
+     * @param ProjectRepository $projectRepository
      * @return Response
      */
-    public function show(Project $project): Response
+    public function show(Project $project, ProjectRepository $projectRepository): Response
     {
+        $projects = $projectRepository->findAll();
+
         return $this->render('project/show.html.twig', [
             'project' => $project,
+            'projects'=>$projects
         ]);
     }
 }
