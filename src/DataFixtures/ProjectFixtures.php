a19af31f5f4523775213436d96fa55a42fabaada
Stephane-acl <stephaneacl@gmail.com>
2020-08-30 16:00:07 +0200

    add condition to display the link 'voir projet'

--- src/DataFixtures/ProjectFixtures.php
+++ src/DataFixtures/ProjectFixtures.php
@@ -20,21 +20,24 @@ class ProjectFixtures extends Fixture implements DependentFixtureInterface
 
     const PROJECTS = [
         'Retro Invaders' => [
-            'description' => 'Retro Invaders est mon premier projet réalisé que j\'ai fait en arrivant à la Wild Code School.
-             C\'est un site qui permet de voir la côte des prix de jeux vidéos rétro.
-             Nous avons développé ce projet en une semaine, c\'était un site statique que j\'ai dynamisé tout au long de ma formation avec du Php.
-             C\'est un projet que j\'aimerais beaucoup retravailler dans le futur.',
+            'description' => 'Retro Invaders est le premier projet que j\'ai réalisé en arrivant à la Wild Code School.
+             C\'est un site qui permet de consulter la côte des jeux vidéos rétros.
+             Nous avons développé ce projet en une semaine.
+             J\'ai dynamisé ce site statique tout au long de ma formation en utilisant du PHP.
+             C\'est un projet que j\'aimerais beaucoup retravailler dans le futur.
+             Il s\'adresse aux passionnés de jeux vidéos.',
 
             'link' => '#',
 
             'client' => 'client_0',
         ],
         'Adventure Couch' => [
-            'description' => "Adventure Couch est un jeu qui permet de déplacer un canapé sur une grille et de faire voyager ce canapé en prenant l'avion. 
-             Mais pour cela il va falloir répondre à des questions.
-             Nous avons développé ce projet à 6 et en 24 heures lors d'un Hackathon organisé par la Wild Code School.
-             Le thème était 'Voyager depuis son canapé'.
-             Nous avons beaucoup utilisé de Jquery et de Php pour développer cette application.",
+            'description' => "Adventure Couch est un jeu qui permet de déplacer un canapé sur une grille et de faire voyager celui-ci en prenant l'avion.
+             En prenant l'avion vous passez au niveau supérieur.
+             Mais pour cela il faudra répondre à des questions.
+             Nous étions 6 personnes pour développer ce projet en 24 heures lors d'un Hackathon organisé par la Wild Code School.
+             Le thème était \"Voyager depuis son canapé\".
+             Nous avons utilisé JQuery et PHP pour développer cette application",
 
             'link' => '#',
 
@@ -42,9 +45,9 @@ class ProjectFixtures extends Fixture implements DependentFixtureInterface
         ],
 
         'SideBySide' => [
-            'description' => "SideBySide est un projet qui permet aux personnes de partager leurs compétences sur des projets en commun.
-             Le site à été développé en 6 semaines à 4. 
-             Nous avons utilisé le simple MVC et du Php pour réaliser cette application.",
+            'description' => "SideBySide est une application permettant de réunir les compétences de différents acteurs pour participer à la réalisation d'un projet commun.
+             Le site a été développé par 4 personnes en 6 semaines.
+             Nous avons utilisé le simple MVC et du PHP pour mettre en œuvre cette application.",
 
             'link' => 'https://sidebysides.herokuapp.com/',
 
@@ -52,11 +55,12 @@ class ProjectFixtures extends Fixture implements DependentFixtureInterface
         ],
 
         'Doctopet' => [
-            'description' => "Doctopet est un pilulier style Tamagochi qui envoie une notification pour rappeler aux enfants de bien prendre leurs médicaments.
-             Nous avons choisi de cibler ce type de patients car un enfant qui est atteint de maladie chronique ne va pas forcément avoir envie de prendre ses médicaments.
-             En faisant cette application nous voulions que l'enfant se responsabilise en prenant soin de son tamagochi et qu'il comprenne que c'est important de prendre ses médicaments pour garder une bonne santé. 
-             Nous avons développé cette application à 4 durant un Hackathon organisé par la Wild Code School et par Doctolib.
-             Nous avions 48 heures pour la réalisation et nous avons utilisé du Php Symfony ainsi que du JQuery.",
+            'description' => "DoctoPet est un pilulier style Tamagochi qui envoie une notification pour rappeler aux enfants de bien prendre leurs médicaments.
+             Nous avons choisi de cibler ce type de patients car un enfant atteint de maladie chronique n'aura pas forcément envie de prendre ses médicaments.
+             C'est une manière ludique pour lui de prendre son traitement sans astreinte.
+             En créant cette application, nous voulions que l'enfant se responsabilise en prenant soin de son tamagochi et comprenne l'importance de prendre ses médicaments pour garder une bonne santé.
+             Nous avons développé cette application à 4 personnes durant un Hackathon organisé par la Wild Code School et DOCTOLIB.
+             Nous disposions de 48 heures pour la réalisation du projet et avons utilisé du PHP Symfony et JQuery.",
 
             'link' => '#',
 
@@ -66,7 +70,7 @@ class ProjectFixtures extends Fixture implements DependentFixtureInterface
         'La Gare Centrale' => [
             'description' => "La Gare Centrale est un projet pour l'association écologique The Greener Good.
              C'est un site à usage interne qui va permettre de guider les nouveaux membres afin de leur faire connaitre les procédures au sein de cette association ( exemple : comment gérer un événement). 
-             Nous avons réalisé cette application à 5 durant 2 mois et demi.
+             Nous avons réalisé cette application à 5 personnes durant 2 mois et demi.
              Nous avons principalement utilisé du Php Symfony pour cette application.",
 
             'link' => 'http://garecentrale.thegreenergood.fr/login',
